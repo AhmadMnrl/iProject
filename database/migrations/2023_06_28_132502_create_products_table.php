336e7428f9b69e4fb414c6e442de5b8e17fd9191
AhmadMnrl <serigalaberbulu465@gmail.com>
2023-06-28 20:29:17 +0700

    three

--- database/migrations/2023_06_28_132502_create_products_table.php
+++ database/migrations/2023_06_28_132502_create_products_table.php
@@ -11,14 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name',100);
-            $table->string('email',100)->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password',50);
-            $table->enum('role',['admin','customer']);
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -28,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 };
